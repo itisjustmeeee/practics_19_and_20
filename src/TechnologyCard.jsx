f3c79ef20229e22d751bf9950a5f4f649224361f
itisjustmeeee <guzairovaolga21@gmail.com>
2025-11-27 03:03:44 +0300

    feat: fixing icons

--- src/TechnologyCard.jsx
+++ src/TechnologyCard.jsx
@@ -1,9 +1,9 @@
 function TechnologyCard() {
     const technologies = [
-        { id: 1, title: 'JSX и React', description: 'Изучение работы JSX и React уомпонентов для контррольной работы', status: 'in-progress' },
-        { id: 2, title: 'SQL', description: 'Изучение функций SQL к контрольной работе', status: 'completed'},
-        { id: 3, title: 'C#', description: 'Изучение базовых функций c# к контрольной работе', status: 'not-started'},
-        { id: 4, title: 'Python', description: 'Изучение языка python для решения задач по ИИ и базам данных', status: 'in-progress'}
+        { id: 1, title: 'JSX и React', description: 'Изучение работы JSX и React уомпонентов для контррольной работы', status: 'in-progress', icon: '(─‿‿─)'},
+        { id: 2, title: 'SQL', description: 'Изучение функций SQL к контрольной работе', status: 'completed', icon: '(¯▿¯)'},
+        { id: 3, title: 'C#', description: 'Изучение базовых функций c# к контрольной работе', status: 'not-started', icon: '(>⩊<)'},
+        { id: 4, title: 'Python', description: 'Изучение языка python для решения задач по ИИ и базам данных', status: 'in-progress', icon: '<(￣︶￣)>'}
     ];
 
     return (
@@ -15,7 +15,7 @@ function TechnologyCard() {
                       key={technologie.id}
                       className={`status ${{"completed": "completed", "in-progress": "in-progress", "not-started": "not-started"}[technologie.status] || "unknown"}`}
                     >
-                        <span>{technologie.title}</span>
+                        <h2>{technologie.title}</h2>
                         <p>{technologie.description}</p>
                     </li>
                 ))}
